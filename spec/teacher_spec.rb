c3b36981e6e256249fac868664e392e2144ce5b7
Ben Kiboma <omayioben@gmail.com>
2022-09-01 21:44:44 +0300

    Test teacher class

---
+++ spec/teacher_spec.rb
@@ -0,0 +1,24 @@
+require 'spec_helper'
+
+describe Teacher do
+  before :each do
+    @teacher = Teacher.new('Python', 37, 'Ben Kiboma')
+  end
+  describe '#new' do
+    it 'teacher should be instance of Teacher class' do
+      expect(@teacher).to be_instance_of Teacher
+    end
+
+    it 'teacher name have the name Ben Kiboma' do
+      expect(@teacher.name).to eq 'Ben Kiboma'
+    end
+
+    it 'teacher should have a Python specialization' do
+      expect(@teacher.specialization).to eq 'Python'
+    end
+
+    it 'teacher age should be 37' do
+      expect(@teacher.age).to be 37
+    end
+  end
+end
